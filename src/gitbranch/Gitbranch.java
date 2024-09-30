7b637febda37df5d1dd8fc7114d9d3953816af07
DDorka02 <dobszaydorka@gmail.com>
2024-09-30 08:57:30 +0200

    alap

--- src/gitbranch/Gitbranch.java
+++ src/gitbranch/Gitbranch.java
@@ -3,12 +3,14 @@ package gitbranch;
 
 
 public class Gitbranch {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Gitbranch().fv();
+    }
+
+    private void fv() {
+        
     }
     
 }
